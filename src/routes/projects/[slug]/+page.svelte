38342f6f14635e855eb2cf09240154e749358fa2
Armin Salihovic <armin.salihovic@live.com>
2022-12-03 02:39:38 +0100

    Add styling for block paragraph

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -13,8 +13,8 @@
     </article>
 </MainLayout>
 
-<!--  <div class="text-lg md:text-5xl text-3xl md:text-5xl text-2xl md:text-4xl text-gray-500"></div>-->
-<!--  <div class="pr-32 xl:pr-32 pr-0 gap-1 gap-2 gap-3 gap-4 gap-5 gap-6 xs:block xs:hidden block"></div>-->
+<!--  <div class="text-lg md:text-3xl md:text-5xl text-3xl md:text-5xl text-2xl md:text-4xl text-gray-500"></div>-->
+<!--  <div class="pb-8 pb-5 md:pb-16 py-8 md:py-16 pr-32 xl:pr-32 pr-0 gap-1 gap-2 gap-3 gap-4 gap-5 gap-6 xs:block xs:hidden block"></div>-->
 <!--  <div class="border border-black p-6 h-24 w-24 w-48 h-48 h-32 h-28 w-28 w-32 w-full lg:w-1/2 flex flex-col lg:flex-row gap-10">-->
 
 
